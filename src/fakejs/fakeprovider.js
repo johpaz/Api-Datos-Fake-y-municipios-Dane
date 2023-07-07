2a65304c2121400ec4c7d6f7c6e96b5aab01ce32
John <johpaz252@gmail.com>
2023-07-07 18:33:50 -0500

    provide

--- src/fakejs/fakeprovider.js
+++ src/fakejs/fakeprovider.js
@@ -88,9 +88,6 @@ const generateProfesionales = async () => {
       ]
     }
   ];
-  const descripcionProfesiones = categorias.flatMap(categoria =>
-    categoria.profesiones.map(profesion => `Soy experto en ${profesion.description}`)
-  );
   
   for (let id = 1; id < 51; id++) {
     const profesional = {
@@ -99,10 +96,9 @@ const generateProfesionales = async () => {
       email: faker.internet.email(),
       image: faker.random.arrayElement(imageUrls),
       genre: faker.random.arrayElement(['male', 'female']),
-      descripcion: descripcionProfesiones.join(' '),
       years_exp: faker.datatype.number({ min: 1, max: 5 }).toString(),
+      description:'',
       categorias: [],
-      descripcion: descripcionProfesiones.join(' '),
       profesiones: [],
       rating: faker.datatype.number({ min: 1, max: 5 }),
       ubicacion: faker.address.city(),
@@ -121,11 +117,15 @@ const generateProfesionales = async () => {
         profesional.profesiones.push({
           id: profesion.id,
           name: profesion.name,
-    
+          description: profesion.description
         });
+
+        if (profesional.description !== '') {
+          profesional.description += ', ';
+        }
+        profesional.description += profesion.description;
       });
     });
-
     profesionales.push(profesional);
   }
 
