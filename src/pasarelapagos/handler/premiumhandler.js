0d2a780804084e573d35ea51a6ecbedab16ebc90
John <johpaz252@gmail.com>
2023-08-02 18:18:52 -0500

    Municipios

--- src/pasarelapagos/handler/premiumhandler.js
+++
@@ -1,8 +0,0 @@
-const {  updatePremiumStatus } = require('../controllers/premiumController');
-
-function premiumHandler(req, res, next) {
-  // Aquí puedes añadir cualquier validación o lógica adicional antes de llamar al controlador
-  updatePremiumStatus(req, res, next);
-}
-
-module.exports = premiumHandler;
--- src/solucion/postProfesionalcontrollers/index.js
+++
@@ -1,6 +0,0 @@
-const createPostProfesional = require("./01 - createPostProfesional");
-const {getAllPostsByProfesionals,getAllPostsByProfesionalsApi, } = require("./02 - getPostsProfesionals");
-const updatePostProfesional = require('./03 - putPostsProfesionals')
-const getPostProfesionalById = require('./04- getPostsProfesionalsById')
-
-module.exports = { createPostProfesional, getAllPostsByProfesionals,getAllPostsByProfesionalsApi,updatePostProfesional,getPostProfesionalById }
